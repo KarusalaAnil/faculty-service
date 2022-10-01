ae92e0ba9052ded81e23acd5ed612a89af8816a6
Karusala Anil <104247266+KarusalaAnil@users.noreply.github.com>
2022-10-01 22:44:10 +0530

    Update FacaltyService.java

--- src/main/java/com/springboot/microservice/service/FacaltyService.java
+++ src/main/java/com/springboot/microservice/service/FacaltyService.java
@@ -26,22 +26,26 @@ public class FacaltyService {
     }
 
     public ServiceResponse<CourseResponseDTO> findCourseWithPathParams(int courseId) {
-        return restTemplate.getForObject(base_URL, ServiceResponse.class, courseId);
+        return restTemplate.getForObject(base_URL+"/find/path/"+courseId, ServiceResponse.class);
+//        return restTemplate.getForObject(base_URL+"/find/path", ServiceResponse.class, courseId);
     }
 
     public ServiceResponse<CourseResponseDTO> findCourseWithRequestParams(Integer courseId) {
         Map<String, Integer> uriVariables = new HashMap<>();
         uriVariables.put("courseId", courseId);
-        return restTemplate.getForObject(base_URL, ServiceResponse.class, uriVariables);
+        return restTemplate.getForObject(base_URL+"/find/request/?courseId={courseId}", ServiceResponse.class,uriVariables);
+//        return restTemplate.getForObject(base_URL+"/find/request", ServiceResponse.class, uriVariables);
     }
 
     public String updateCourseDetails(Integer courseId, CourseRequestDTO requestDTO) {
-        restTemplate.put(base_URL + courseId, requestDTO);
+//        restTemplate. put(base_URL +"/update", requestDTO,courseId);
+        restTemplate. put(base_URL +"/update/"+courseId, requestDTO);
         return "Course Id Updated with " + courseId;
     }
 
     public String deleteCourseDetails(Integer courseId) {
-        restTemplate.delete(base_URL+courseId);
+        restTemplate.delete(base_URL+"/"+courseId);
+//        restTemplate.delete(base_URL+courseId);
         return "Course Deleted Successfully with "+courseId;
     }
 }
